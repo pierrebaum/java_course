967906d4b177d436a9a0fd872a3078b688ad369f
pierrebaum <pierre.a.baum@gmail.com>
2020-03-24 12:49:35 +0100

    Transformed 100 pennies to 1 Dollar in calculation

--- src/chapter3/ChangeDollarGame.java
+++ src/chapter3/ChangeDollarGame.java
@@ -8,14 +8,15 @@ public class ChangeDollarGame {
 
     // Initialize what we know
 
-    int value_quarter = 25;
-    int value_dime = 10;
-    int value_nickel = 5;
-    int expected_result = 100;
-    int result;
-    int difference_result;
+    double value_quarter = 0.25;
+    double value_dime = 0.10;
+    double value_nickel = 0.05;
+    double value_penny = 0.01;
+    int dollar = 1;
 
     // Ask for items we don't know
+        System.out.println("Welcome to 'Change for a Dollar'!" +
+                "Your goal is to enter enough change to make exactly $1.00");
     System.out.println("How many quarters would you like to add?");
     Scanner scanner = new Scanner(System.in);
     int quarters = scanner.nextInt();
@@ -27,18 +28,19 @@ public class ChangeDollarGame {
     int pennies = scanner.nextInt();
     scanner.close();
     // Calculate the value
-    result = quarters * value_quarter + dimes * value_dime + nickels * value_nickel + pennies;
-    difference_result = expected_result - result;
+    double result = quarters * value_quarter + dimes * value_dime + nickels * value_nickel + pennies * value_penny;
+
     // Make a decision
-        if (difference_result==0){
-            System.out.println("Well done! You won");
+        if (result < dollar){
+            double result_short = dollar - result;
+            System.out.println("You lost! You are short by $" + String.format("%.2f", result_short));
         }
-        else if(difference_result > 0){
-            System.out.println("You lost! You are short by " + difference_result + " pennies");
+        else if(result > dollar){
+            double result_over = result - dollar;
+            System.out.println("You lost! You gave $" + String.format("%.2f", result_over) + " too much");
         }
         else{
-            difference_result = difference_result * -1;
-            System.out.println("You lost! You gave " + difference_result + " pennies too much.");
+            System.out.println("Well done! You won");
         }
 
     }
